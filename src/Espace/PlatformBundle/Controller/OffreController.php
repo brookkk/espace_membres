a3f9dc8e3548059331634cd3449e9697d82bd415
brook <grini.abdellah@gmail.com>
2018-03-14 17:37:40 +0000

    offre cru

--- src/Espace/PlatformBundle/Controller/OffreController.php
+++ src/Espace/PlatformBundle/Controller/OffreController.php
@@ -57,4 +57,78 @@ class OffreController extends Controller
      ));
 
   }
+
+
+  //Action pour Afficher toutes les offres existantes
+  public function show_offreAction(Request $request)
+  {
+    $em= $this  ->getDoctrine()  ->getManager();
+
+    $repository = $em  ->getRepository('EspacePlatformBundle:Offre');
+    
+
+    $listOffres = $repository->findAll();
+
+    if (null === $listOffres) {
+      throw new NotFoundHttpException("Aucune Offre na été trouvé");
+    }
+
+
+    return $this->render('EspacePlatformBundle:Show:offre.html.twig', array(
+      'listOffres' => $listOffres   ));
+  }
+
+
+
+  public function update_offreAction(Request $request, $id)
+  {
+
+    $repository = $this  ->getDoctrine()  ->getManager()  ->getRepository('EspacePlatformBundle:Offre');
+
+    $offre = $repository->find($id);
+
+
+
+ 
+
+    if (null === $offre) {
+      throw new NotFoundHttpException("Votre offre na pas été trouvée");
+    }
+
+
+    $form = $this->createForm(OffreType::class, $offre);
+
+
+
+    if($request->isMethod('POST')){
+
+      $form->handleRequest($request);
+
+      if($form->isValid()){
+        $em= $this->getDoctrine()->getManager();
+        $em->persist($offre);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Offre Bien enregistré.');
+
+       
+
+
+        return $this->redirectToRoute('EP_show_offre');
+      }
+
+
+
+
+    }
+
+    return $this->render('EspacePlatformBundle:New:offre.html.twig', array(
+     'form'=>$form->createView(),
+     ));
+
+
+  }
+
+
+
 }
