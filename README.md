215321a0f1dba9f6fc5363fd1e7c946cc414e382
brook <grini.abdellah@gmail.com>
2018-03-14 14:21:36 +0000

    espace first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+espace_membres
+==============
+
+A Symfony project created on March 14, 2018, 10:57 am.
---
+++ src/Espace/PlatformBundle/EspacePlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Espace\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EspacePlatformBundle extends Bundle
+{
+}
