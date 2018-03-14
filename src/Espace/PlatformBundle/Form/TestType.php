6ba4ac278e2f9d2a5d7a55cb5a00bb04e82036e9
brook <grini.abdellah@gmail.com>
2018-03-14 17:59:24 +0000

    offre cru

---
+++ src/Espace/PlatformBundle/Form/TestType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Espace\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TestType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Espace\PlatformBundle\Entity\Test'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'espace_platformbundle_test';
+    }
+
+
+}
