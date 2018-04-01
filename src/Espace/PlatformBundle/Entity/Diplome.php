78f2c5236b6973e1a267e7e0ceb375f12868eba8
brookkk <grini.abdellah@gmail.com>
2018-04-02 00:59:12 +0100

    alumni diplome

---
+++ src/Espace/PlatformBundle/Entity/Diplome.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Espace\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Niveau_de_formation
+ *
+ * @ORM\Table(name="diplome")
+ * @ORM\Entity(repositoryClass="Espace\PlatformBundle\Repository\DiplomenRepository")
+ */
+class Diplome
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=true, unique=true)
+     */
+    private $nom;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Niveau_de_formation
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
+
