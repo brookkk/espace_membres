76874fec53b10ba80bc29533945da48a8ebe734f
brookkk <grini.abdellah@gmail.com>
2018-04-06 00:08:01 +0100

    captcha 01

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Espace\PlatformBundle\EspacePlatformBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Espace\UserBundle\EspaceUserBundle(),
+            new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
