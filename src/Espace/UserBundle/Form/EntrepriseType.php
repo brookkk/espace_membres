b28d5d4b37756bfa21cc79f58e58c15791b130d7
brookkk <grini.abdellah@gmail.com>
2018-04-06 00:25:43 +0100

    captcha

--- src/Espace/UserBundle/Form/EntrepriseType.php
+++ src/Espace/UserBundle/Form/EntrepriseType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
 
 
 class EntrepriseType extends AbstractType
@@ -56,6 +57,7 @@ public function buildForm(FormBuilderInterface $builder, array $options)
             ))
         ->add('votreFonction')
         ->add('newsletter')
+        ->add('captcha', CaptchaType::class)
         ->add('Sauvegarder', SubmitType::class);
     }
 
