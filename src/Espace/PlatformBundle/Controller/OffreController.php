24aacc6f9a80038d87337cbdef0dd0f0e260bc37
brookkk <grini.abdellah@gmail.com>
2018-04-09 01:44:53 +0100

    postuleurs

--- src/Espace/PlatformBundle/Controller/OffreController.php
+++ src/Espace/PlatformBundle/Controller/OffreController.php
@@ -370,10 +370,22 @@ public function delete_offreAction(Request $request, $id)
 
     $user = $this->getUser();
 
-    if(  in_array($user, $offre->getUsers()->toArray())         )
-      return 1;
-    else return 0;
-    
+    //print_r($offre->getUsers()->toArray());
+    $ids = array();
+    foreach ($offre->getUsers() as $us){
+      $ids[] = $us->getId();
+    }
+    /*echo("<br> id : " .$user->getId());
+    echo("<br> ids");
+
+    print_r($ids);*/
+
+    if(  in_array($user->getId(), $ids)         )
+      $return=1;
+    else $return= 0;
+
+    echo("return : " .$return);
+    return $return;
 
 
   }
