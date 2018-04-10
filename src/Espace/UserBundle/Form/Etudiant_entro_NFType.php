38e88915b959f470cce9eaee7074fd085574f717
brook <grini.abdellah@gmail.com>
2018-04-10 15:52:03 +0100

    candidat two steps NF

--- src/Espace/UserBundle/Form/Etudiant_entro_NFType.php
+++ src/Espace/UserBundle/Form/Etudiant_entro_NFType.php
@@ -31,87 +31,16 @@ public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
         $builder
-        ->add('civilite', ChoiceType::class, array(
-            'label'=> "Civilité",
-            'choices' => array(
-                 'Homme' => true,
-                 'Femme' => false,
-                 ),
-            'expanded' => true,
-            'multiple' => false,
-            ))
-        ->add('nom')->add('prenom')
-        
-         ->add('adresse')->add('codePostal')->add('ville')->add('email', EmailType::class)
-         ->add('telephone')
-         ->add('ddn', DateType::class, array(
-            'widget' => 'choice',
-            'years' => range(2017,1940),
-            'format' => 'dd-MM-yyyy',
-            'label'=> 'Date de naissance'
-            ))
-         ->add('numIne', TextType::class, array(
-            'label'=> 'Numéro INE'
-            ))
-        ->add('niveauFormation', EntityType::class, array(
-                'class'        => 'EspacePlatformBundle:Niveau_de_formation',
-                'choice_label' => 'nom',
-                'multiple'     => false,
-                ))
-        ->add('diplome', EntityType::class, array(
-                'class'        => 'EspacePlatformBundle:Diplome',
-                'choice_label' => 'nom',
-                'multiple'     => true,
-                'label'        => "Titre du diplome faisant objet de l'inscription"
-                ))
-        ->add('pcNom', TextType::class, array(
-            'label'=> 'Nom'
-            ))
-        ->add('pcPrenom', TextType::class, array(
-            'label'=> 'Prenom'
-            ))
-        ->add('pcCodePostal', IntegerType::class, array(
-            'label'=> 'Code postal'
-            ))
-        ->add('pcVille', TextType::class, array(
-            'label'=> 'Ville'
-            ))
-        ->add('pcEmail', TextType::class, array(
-            'label'=> 'Email'
-            ))
-        ->add('pcTelephone', TextType::class, array(
-            'label'=> 'Numéro de téléphone'
-            ))
-        ->add('pcTelephonePortable', TextType::class, array(
-            'label'=> 'Numéro de téléphone portable'
-            ))
-        ->add('pcLien', TextType::class, array(
-            'label'=> "Lien avec l'intéressé" 
-            ))
-        
-        ->add('foundEntro', ChoiceType::class, array(
-            'label'=> "Avez-vous trouvé une entreprise ?",
+        ->add('setServiceAccompagnement', ChoiceType::class, array(
+            'label'=> "Souhaitez-vous profiter du service d’accompagnement à la recherche du poste d'alternant ?",
             'choices' => array(
                  'Oui' => true,
-                 'Nom' => false,
+                 'Non' => false,
                  ),
             'expanded' => true,
             'multiple' => false,
             ))
-        ->add('newsletter', ChoiceType::class, array(
-            'label'=> "J’accepte de recevoir la newsletter",
-            'choices' => array(
-                 'Oui' => true,
-                 'Nom' => false,
-                 ),
-            'expanded' => true,
-            'multiple' => false,
-            ))
-        ->add('plainPassword', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Mot de Passe'),
-                'second_options' => array('label' => 'Confirmer votre mot de passe'),
-            ))
+       
         ->add('Sauvegarder', SubmitType::class);
     }
 
