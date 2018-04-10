4d8cde4e7f654d34dd90017a20eba23ca6a2ebc1
brook <grini.abdellah@gmail.com>
2018-04-10 17:17:32 +0100

    candidat entité CV

--- src/Espace/UserBundle/Entity/Cv.php
+++ src/Espace/UserBundle/Entity/Cv.php
@@ -9,11 +9,30 @@ use Espace\UserBundle\Entity\User;
 /**
  * Cv
  *
- * @ORM\Table(name="etudiant")
+ * @ORM\Table(name="cv")
  * @ORM\Entity(repositoryClass="Espace\UserBundle\Repository\CvRepository")
  */
 class Cv 
 {
+
+
+ /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+
+/**
+     * @var string
+     *     
+     * @ORM\Column(name="mobilite", type="integer",  nullable=true)
+     */
+    private $mobilite;
     
 }
 
