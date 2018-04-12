829ec5b480dc597a9cdac61512799ff50092d557
brook <grini.abdellah@gmail.com>
2018-04-12 15:56:40 +0100

    dynamic form

---
+++ src/Espace/PlatformBundle/Form/TaskType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Espace\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Espace\PlatformBundle\Entity\Task;
+
+class TaskType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('description')
+        ->add('tags', CollectionType::class, array(
+            'entry_type' => TagType::class,
+            'entry_options' => array('label' => false),
+        ));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Espace\PlatformBundle\Entity\Task'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'espace_platformbundle_task';
+    }
+
+
+}
