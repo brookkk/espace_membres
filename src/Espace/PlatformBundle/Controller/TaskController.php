31da723abaa11b9f6e1de476425f92a16012df17
brook <grini.abdellah@gmail.com>
2018-04-12 18:26:58 +0100

    dynamic form add remove working example

--- src/Espace/PlatformBundle/Controller/TaskController.php
+++ src/Espace/PlatformBundle/Controller/TaskController.php
@@ -5,6 +5,7 @@ namespace Espace\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\Common\Collections\ArrayCollection;
 
 use Espace\PlatformBundle\Entity\Task;
 use Espace\PlatformBundle\Entity\Tag;
@@ -31,12 +32,12 @@ class TaskController extends Controller
 
         // dummy code - this is here just so that the Task has some tags
         // otherwise, this isn't an interesting example
-        $tag1 = new Tag();
+      /*  $tag1 = new Tag();
         $tag1->setName('tag1');
         $task->getTags()->add($tag1);
         $tag2 = new Tag();
         $tag2->setName('tag2');
-        $task->getTags()->add($tag2);
+        $task->getTags()->add($tag2);*/
         // end dummy code
 
         $form = $this->createForm(TaskType::class, $task);
@@ -77,6 +78,63 @@ class TaskController extends Controller
    // return $this->render('EspaceUserBundle:Show:index.html.twig');
 
   }
+
+
+
+
+
+
+
+
+
+
+  public function editAction($id, Request $request)
+{
+    $entityManager = $this->getDoctrine()->getManager();
+    $task = $entityManager->getRepository(Task::class)->find($id);
+
+    if (!$task) {
+        throw $this->createNotFoundException('No task found for id '.$id);
+    }
+
+    $originalTags = new ArrayCollection();
+
+    // Create an ArrayCollection of the current Tag objects in the database
+    foreach ($task->getTags() as $tag) {
+        $originalTags->add($tag);
+    }
+
+    $editForm = $this->createForm(TaskType::class, $task);
+
+    $editForm->handleRequest($request);
+
+    if ($editForm->isValid()) {
+
+        // remove the relationship between the tag and the Task
+        foreach ($originalTags as $tag) {
+            if (false === $task->getTags()->contains($tag)) {
+                // remove the Task from the Tag
+                $task->getTags()->removeElement($tag);
+
+           
+                $entityManager->persist($tag);
+
+                // if you wanted to delete the Tag entirely, you can also do that
+                // $entityManager->remove($tag);
+            }
+        }
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        // redirect back to some edit page
+        return $this->redirectToRoute('EP_task_edit', array('id' => $id));
+    }
+
+    return $this->render('EspacePlatformBundle:New:task.html.twig', array(
+            'form' => $editForm->createView(),
+        ));
+}
 
 
 
