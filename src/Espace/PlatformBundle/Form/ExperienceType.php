0d319aaf6e5a99e369edb09b772c3b6411b7f3dc
brook <grini.abdellah@gmail.com>
2018-04-12 18:51:32 +0100

    dynamic CV experience

---
+++ src/Espace/PlatformBundle/Form/ExperienceType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Espace\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Espace\PlatformBundle\Entity\Experience;
+
+class TagType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('anneeDebut')
+                ->add('anneeFin')
+                ->add('entreprise')
+                ->add('ville')
+                ->add('codePostal')
+                ->add('posteOccupe')
+                ->add('missions');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Espace\PlatformBundle\Entity\Experience'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'espace_platformbundle_tag';
+    }
+
+
+}
