5491a08bb76be7acf32e88cae0fbf3d3706c10d1
brookkk <grini.abdellah@gmail.com>
2018-04-15 22:30:31 +0100

    user crud update issue

--- src/Espace/UserBundle/Controller/UserController.php
+++ src/Espace/UserBundle/Controller/UserController.php
@@ -8,10 +8,17 @@ use Symfony\Component\HttpFoundation\Request;
 //use Espace\UserBundle\Entity\User;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+use Espace\UserBundle\Entity\Etudiant;
+use Espace\UserBundle\Entity\User;
+use Espace\UserBundle\Entity\Cv;
 
-
+use Espace\UserBundle\Form\EtudiantType;
+use Espace\UserBundle\Form\Etudiant_entro_NFType;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Espace\UserBundle\Entity\Entreprise;
+
+use Espace\UserBundle\Form\EntrepriseType;
 
 
 
@@ -133,6 +140,66 @@ class UserController extends Controller
 
   }
 
+
+
+
+
+ public function update_userAction(Request $request, $id)
+  {
+
+    $repository = $this  ->getDoctrine()  ->getManager()  ->getRepository('EspaceUserBundle:User');
+
+    $user = $repository->find($id);
+
+
+/*
+ 
+
+    if (null === $user) {
+      throw new NotFoundHttpException("Votre user na pas été trouvée");
+    }
+
+if($user->getProfile()=='ETUDIANT')
+    $form = $this->createForm(EtudiantType::class, $user);
+
+elseif($user->getProfile()=='ENTREPRISE')
+    $form = $this->createForm(EntrepriseType::class, $user);
+
+
+    if($request->isMethod('POST')){
+
+      $form->handleRequest($request);
+
+      if($form->isValid()){
+        $em= $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Offre Bien enregistré.');
+
+       
+
+
+        return $this->redirectToRoute('espace_platform_homepage');
+      }
+
+
+
+
+    }
+*/
+
+if($user->getProfile()=='ETUDIANT')
+      return $this->render('EspaceUserBundle:New:etudiant.html.twig', array(
+           'form'=>$form->createView(),
+           ));
+elseif($user->getProfile()=='ENTREPRISE')
+     return $this->render('EspaceUserBundle:New:entreprise.html.twig', array(
+     'form'=>$form->createView(),
+     ));
+
+
+  }
 
 
 }
