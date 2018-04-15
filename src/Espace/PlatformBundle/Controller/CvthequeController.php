f7afd568b055c92d3733e45cfd51d5ee550a0d36
brookkk <grini.abdellah@gmail.com>
2018-04-16 00:59:43 +0100

    cvtheque

--- src/Espace/PlatformBundle/Controller/CvthequeController.php
+++ src/Espace/PlatformBundle/Controller/CvthequeController.php
@@ -6,9 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use Espace\PlatformBundle\Entity\Offre;
-use Espace\PlatformBundle\Form\OffreType;
-use Espace\PlatformBundle\Form\OffreSearchType;
+use Espace\UserBundle\Entity\User;
+use Espace\UserBundle\Form\EtudiqntSearchType;
 
 
 
@@ -18,6 +17,68 @@ class CvthequeController extends Controller
     {
         return $this->render('EspacePlatformBundle:Default:admin.html.twig');
     }
+
+
+
+
+/**
+   * @Security("has_role('ROLE_ENTREPRISE')")
+   * 
+   */
+
+     public function searchAction(Request $request)
+  {
+//nouvelle instance de l'entité Offre
+    $user= new User();
+
+ 
+
+    $form = $this->createForm(UserSearchType::class, $user);
+
+
+//si le formulaire est bien rempli, on l'enregistre dans la BD
+    if($request->isMethod('POST')){
+
+      $form->handleRequest($request);
+
+ 
+      if($form->isValid()   
+        ){
+        
+
+      //print_r($offre);
+
+
+    $em= $this  ->getDoctrine()  ->getManager();
+
+    $repository = $em  ->getRepository('EspacePlatformBundle:Offre');
+    
+
+    $listOffres = $repository->findAll();
+
+    $listOffres = $repository->findBy([
+      'niveauFormationP' => $offre->getNiveauFormationP() ,
+    ]);
+
+
+
+
+    return $this->render('EspacePlatformBundle:Show:offre.html.twig', array(      'listOffres' => $listOffres   ));
+      }
+    }
+
+//sinon (ou bien premier landing sur le form), on affiche le formulaire
+     $em= $this  ->getDoctrine()  ->getManager();
+
+    $repository = $em  ->getRepository('EspacePlatformBundle:Offre');
+    
+
+    $listOffres = $repository->findAll();
+    return $this->render('EspacePlatformBundle:New:offre_search.html.twig', array(
+     'form'=>$form->createView(), 'listOffres' => $listOffres
+     ));
+
+  }
 
 
 
---
+++ src/Espace/UserBundle/Form/EtudiantSearchType.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Espace\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class EtudiantType extends AbstractType
+{
+/**
+     * {@inheritdoc}
+     */
+
+
+
+public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+        ->add('civilite', ChoiceType::class, array(
+            'label'=> "Civilité",
+            'choices' => array(
+                 'Homme' => true,
+                 'Femme' => false,
+                 ),
+            'expanded' => true,
+            'multiple' => false,
+            ))
+        ->add('nom')->add('prenom')
+        
+         ->add('adresse')->add('codePostal')->add('ville')->add('email', EmailType::class)
+         ->add('telephone')
+         ->add('ddn', DateType::class, array(
+            'widget' => 'choice',
+            'years' => range(2017,1940),
+            'format' => 'dd-MM-yyyy',
+            'label'=> 'Date de naissance'
+            ))
+         ->add('numIne', TextType::class, array(
+            'label'=> 'Numéro INE'
+            ))
+        ->add('niveauFormation', EntityType::class, array(
+                'class'        => 'EspacePlatformBundle:Niveau_de_formation',
+                'choice_label' => 'nom',
+                'multiple'     => false,
+                ))
+        ->add('diplome', EntityType::class, array(
+                'class'        => 'EspacePlatformBundle:Diplome',
+                'choice_label' => 'nom',
+                'multiple'     => true,
+                'expanded'     => true,
+                'label'        => "Titre du diplome faisant objet de l'inscription"
+                ))
+        ->add('pcNom', TextType::class, array(
+            'label'=> 'Nom'
+            ))
+        ->add('pcPrenom', TextType::class, array(
+            'label'=> 'Prenom'
+            ))
+        ->add('pcCodePostal', IntegerType::class, array(
+            'label'=> 'Code postal'
+            ))
+        ->add('pcAdresse', TextType::class, array(
+            'label'=> 'Adresse'
+            ))
+        ->add('pcVille', TextType::class, array(
+            'label'=> 'Ville'
+            ))
+        ->add('pcEmail', TextType::class, array(
+            'label'=> 'Email'
+            ))
+        ->add('pcTelephone', TextType::class, array(
+            'label'=> 'Numéro de téléphone'
+            ))
+        ->add('pcTelephonePortable', TextType::class, array(
+            'label'=> 'Numéro de téléphone portable'
+            ))
+        ->add('pcLien', TextType::class, array(
+            'label'=> "Lien avec l'intéressé" 
+            ))
+        
+        ->add('foundEntro', ChoiceType::class, array(
+            'label'=> "Avez-vous trouvé une entreprise ?",
+            'choices' => array(
+                 'Oui' => true,
+                 'Nom' => false,
+                 ),
+            'expanded' => true,
+            'multiple' => false,
+            ))
+        ->add('newsletter', ChoiceType::class, array(
+            'label'=> "J’accepte de recevoir la newsletter",
+            'choices' => array(
+                 'Oui' => true,
+                 'Nom' => false,
+                 ),
+            'expanded' => true,
+            'multiple' => false,
+            ))
+        ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Mot de Passe'),
+                'second_options' => array('label' => 'Confirmer votre mot de passe'),
+            ))
+        ->add('Sauvegarder', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Espace\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'espace_userbundle_etudiant';
+    }
+
+
+}
