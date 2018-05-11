1f6fa32520e32bc68401175dde30395f493738b8
brookkk <grini.abdellah@gmail.com>
2018-05-11 16:13:18 +0100

    etudiant logiciel

---
+++ src/Espace/PlatformBundle/Entity/Logiciel.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Espace\PlatformBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Langue
+ *
+ * @ORM\Table(name="logiciel")
+ * @ORM\Entity(repositoryClass="Espace\PlatformBundle\Repository\LangueRepository")
+ */
+class Logiciel
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+
+
+
+
+
+
+
+
+
+    /**     
+     * @var string      
+     *      
+     * @ORM\Column(name="langue", type="string", length=255)       
+     */     
+    private $logiciel;  
+
+
+
+ 
+
+
+         /**
+     * @var string
+     *
+     * @ORM\Column(name="niveau", type="integer",  nullable=true)
+     */
+    private $niveau;    
+
+
+
+  
+       public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+ 
+
+
+     public function getLogiciel()
+    {
+        return $this->logiciel;
+    }
+
+    public function setLogiciel($logiciel)
+    {
+        $this->logiciel = $logiciel;
+    }
+
+
+   public function getNiveau()
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau($niveau)
+    {
+        $this->niveau = $niveau;
+    }
+
+  
+   
+
+
+}
+
