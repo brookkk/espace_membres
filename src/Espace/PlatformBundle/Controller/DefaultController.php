b7338a530b1a7da6d55492f9c08ff0d70e6ec276
brookkk <grini.abdellah@gmail.com>
2018-05-28 14:55:11 +0000

    mailer vpn issue ?

--- src/Espace/PlatformBundle/Controller/DefaultController.php
+++ src/Espace/PlatformBundle/Controller/DefaultController.php
@@ -17,16 +17,16 @@ class DefaultController extends Controller
 
     public function send_mailAction()
     {
-    	for($i=0;$i<1;$i++){
+    	
     	 $message = \Swift_Message::newInstance()
-        ->setSubject('Hello Email'.($i+1))
+        ->setSubject('Hello Email')
         ->setFrom('sim.rmay.no3@gmail.com')
         ->setTo('grini.abdellah@gmail.com')
         ->setBody("espace test !!");
         $this->get('mailer')->send($message);
-}
 
- $this->get('mailer')->send($message);
+
+// $this->get('mailer')->send($message);
      //$this->getMailer()->send("tets");
 
      return $this->render('EspacePlatformBundle:Default:index.html.twig');
